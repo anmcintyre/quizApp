e87eb8234babab9cab0896a08e52e37c0894b292
anmcintyre <andrea.mcintyre@gmail.com>
2015-05-19 14:22:41 -0400

    made the pages functional

--- js/main.js
+++ js/main.js
@@ -1,4 +1,52 @@
 $(document).ready(function(){
-	$("#tabs").tabs(); 
-	$("button" ).button();
-})
+
+	$("#tabs").tabs({
+		disabled: [1, 2, 3, 4, 5]
+	}); 
+	
+	$("button" ).button().click(function(){
+		if (showingQuestion)
+			showAnswer(tabIndex+1);
+		else
+			nextQuestion();
+		showingQuestion = !showingQuestion;		
+	});
+
+});
+
+var tabIndex = 0;
+var showingQuestion = true;
+var answers = ["d", "a", "c", "b", "d"];
+var correctAnswers = 0;
+
+function showAnswer(questionNum){
+	var answer = $("input[name=questionRadio" + questionNum + "]:checked").val();
+	if (answer == undefined){
+		alert("Please select an answer");
+		return;
+	}
+
+	if (answer === answers[questionNum-1]){
+		$("h2", "#answer" + questionNum).text("Correct!");
+		correctAnswers++;
+	} else {
+		$("h2", "#answer" + questionNum).text("Incorrect");
+	}
+	
+	$("#query" + questionNum).hide();
+	$("#answer" + questionNum).show();
+}
+
+function nextQuestion(){
+	if (tabIndex == 4){
+		$("#correctNum").text(correctAnswers);	
+		$("button").remove();
+	}
+	$("#tabs").tabs("enable", tabIndex+1)
+		.tabs("disable", tabIndex)
+		.tabs("option", "active", tabIndex+1);
+	tabIndex++;
+}
+
+
+
